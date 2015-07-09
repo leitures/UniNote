04103c9aadcbbee319172eab0b4f1e40cc6ccdcb
Sysan <shifanglei7@Gmail.com>
2015-07-09 16:25:12 +0800

    登陆/注册

--- uniNOTE/js/materialize.js
+++ uniNOTE/js/materialize.js
@@ -6158,3 +6158,8 @@ Picker.extend( 'pickadate', DatePicker )
   });
 
 }( jQuery ));
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
